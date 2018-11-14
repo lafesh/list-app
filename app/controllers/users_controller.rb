ee2b19c6060456da1dbc400da064fa195c732102
lafesh <ginafedder@yahoo.com>
2018-11-14 11:52:27 -0700

    created a homepage.erb and login request with form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,27 @@ class UsersController < Sinatra::Base
     post '/signup' do
         @user = User.new(params)
         if @user.save
+            @lists = User.lists.all
             session[:user_id] = user.id 
             erb :homepage 
         else
             flash[:message] = "Please fill out all the fields"
             redirect '/signup'
+        end
+    end
+
+    get '/login' do
+        erb :'users/login'
+    end
+
+    post '/login' do
+        @user = User.find_by_id(params[:id])
+        if @user && @user.authenticate(params[:password])
+            @lists = User.lists.all
+            erb :homepage
+        else 
+            flash[:message] = "Please fill out all the fields"
+            redirect '/login'
         end
     end
 end
