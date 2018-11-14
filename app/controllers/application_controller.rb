e8bef9e6fa6bb160f66c6c336fdcd7ccb8eaf0bc
lafesh <ginafedder@yahoo.com>
2018-11-14 11:26:05 -0700

    added to configure and created helpers in application conntroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions #flash messages
+    set :session_secret, "password_security" #bcrypt? I think
   end
 
   get "/" do
-    erb :welcome
+    erb :index
   end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+end
 
 end
