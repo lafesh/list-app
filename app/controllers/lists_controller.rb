4a43b582fb053589da61c8809706f9da1b69e073
lafesh <ginafedder@yahoo.com>
2018-11-14 23:28:14 -0700

    created get, post for new list and created new.erb with a create list form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
     get '/lists' do
         erb :'lists/homepage'
     end
+
+    get '/lists/new' do
+        redirect '/login' unless logged_in?
+        erb :'lists/new'
+    end
+
+    post '/lists' do
+        list = List.new(params)
+        if list.save
+            redirect '/lists'
+        else 
+            flash[:message] = "Please fill out all the fields available."
+            redirect '/lists/new'
+        end
+    end
 end
